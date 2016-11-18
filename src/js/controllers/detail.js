f26951ab6ce0467c8a9c8b6934505150b302d0b7
Grace Lee <eunheh@gmail.com>
2016-11-18 16:49:12 -0500

    love button works

--- src/js/controllers/detail.js
+++ src/js/controllers/detail.js
@@ -8,12 +8,22 @@ function DetailController ($scope, $http, $stateParams, $document) {
    let url = SERVER + '/post/' + $stateParams.id;
    $http.get(url).then((resp) => {
      $scope.img = resp.data;
+     $scope.times = ($scope.img.count === null || $scope.img.count === 1) ? '' : '\'s';
+     console.log($scope.img)
    });
  };
 
 $scope.addLike = (img) => {
- 
     img.count++
+    // console.log($document.find('button').eq(0));
+    $document.find('button').eq(0).addClass('loved');
+      setTimeout(()=>{
+        $document.find('button').eq(0).removeClass('loved');
+      }, 1000);
+
+    if (img.count === 1) $scope.times = '';
+    else $scope.times = '\'s';
+
     $http.put(SERVER + '/post/' + $stateParams.id, img)
     // $document.find('span').eq().addClass('count');
   };
