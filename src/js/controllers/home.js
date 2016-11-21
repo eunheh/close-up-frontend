d38e776098c844c2f95879dd9b31a09d777447b4
Grace Lee <eunheh@gmail.com>
2016-11-20 22:02:17 -0500

    jeremy's changes

--- src/js/controllers/home.js
+++ src/js/controllers/home.js
@@ -2,17 +2,8 @@ import { SERVER } from "../server";
 
 function HomeController ($scope, $http, $stateParams, $document) {
   $scope.imgs = [];
-<<<<<<< HEAD
   $scope.img = {};
-  
-=======
-  	$scope.errors = {
-		name:"Title is Requried!",
-		Link:"Link cannot be left empty!"
 
-	};
-
->>>>>>> 52b2ac5380153c9d372d12f7382bc6fc8befbc5f
   function init () {
     $http.get(SERVER + '/post').then((resp) => {
       $scope.imgs = resp.data;
@@ -34,32 +25,7 @@ function HomeController ($scope, $http, $stateParams, $document) {
 
   init();
 
-<<<<<<< HEAD
 };
 
 HomeController.$inject = ['$scope', '$http', '$stateParams', '$document'];
-=======
-   $scope.validateName = function (name){
-  	// console.log(title)
-      if (name === ''){
-		$scope.errors.name = "Title cannot be left empty!"
-      }else{
-      	$scope.errors.name =''
-      }
-  };
-
-$scope.validateEmail = function (email){
-  	// console.log(title)
-      if (email === ''){
-		$scope.errors.email = "Link cannot be left empty!"
-      }else{
-      	$scope.errors.email =''
-      }
-  };
-
-};
-
-
-HomeController.$inject = ['$scope', '$http'];
->>>>>>> 52b2ac5380153c9d372d12f7382bc6fc8befbc5f
 export { HomeController };
